b95d83ee7aa33180973ee672f39d106ad29d6fa3
Kavyaalungal <142202840+Kavyaalungal@users.noreply.github.com>
2024-06-04 14:31:37 +0530

    Add files via upload

--- Registration.jsx
+++ Registration.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel'; // Add this line
+import Select from '@mui/material/Select'; // Add this line
+import MenuItem from '@mui/material/MenuItem'; // Add this line
+import { FormControl } from '@mui/material';
 import './App.css';
 import './Registration.css';
 import { Link } from 'react-router-dom';
@@ -55,32 +59,83 @@ function Registration() {
       <fieldset className='fieldset'>
         <legend>Search</legend>
         <div className='search'>
-          <TextField id="outlined-basic" label="Search" className='search' variant="outlined" size="small" />
-          <button className='button'>Search</button>
-        </div>
-        <div className="uhid">
-          <TextField id="outlined-basic" label="UHID" variant="outlined" size="small" />
-        </div>
+  <TextField
+    id="outlined-basic"
+    label="Search"
+    className='search'
+    variant="outlined"
+    size="small"
+    InputProps={{
+      style: { height: '40px', width: '200px' },
+   
+    }}
+    InputLabelProps={{ style: { fontSize: '14px', marginTop: '-3px' } }}
+  />
+  <button className='button'>Search</button>
+</div>
+        
       </fieldset>
+      <div className="uhid">
+          <TextField id="outlined-basic" label="UHID" variant="outlined" size="small"
+           InputProps={{ style: { height: '30px' ,width:'150px' } }}
+           InputLabelProps={{ style: { fontSize: '14px', marginTop: '-5px' } }} />
+        </div>
       <fieldset className='fieldset'>
         <legend>Patient details:</legend>
         <form>
           <div className='select input-row'>
-            <select name="" id="" onChange={handlePrefixChange}>
+            
+      <FormControl sx={{ m: 1, minWidth: 80 }}>
+      <InputLabel id="demo-simple-select-autowidth-label">Prefix</InputLabel>
+          <Select
+  labelId="demo-simple-select-label"
+  id="demo-simple-select"
+  value={gender ? `${gender}:` : ''} 
+  label="Prefix"
+  onChange={handlePrefixChange}
+>
+  <MenuItem value="MR:">MR:</MenuItem>
+  <MenuItem value="MRS:">MRS:</MenuItem>
+  <MenuItem value="MISS:">MISS:</MenuItem>
+</Select>
+</FormControl>
+ {/* <select name="" id="" onChange={handlePrefixChange}>
               <option value="">---</option>
               <option value="MR:">MR:</option>
               <option value="MRS:">MRS:</option>
               <option value="MISS:">MISS:</option>
-            </select>
-            <TextField className='name' id="outlined-basic" label="Enter your name" variant="outlined" size="small" />
+            </select> */}
+            <TextField className='name' id="outlined-basic" label="Enter your name" variant="outlined" size="small"
+              InputProps={{ style: { width:'650px' } }}
+              InputLabelProps={{ style: { fontSize: '14px', marginTop: '-1px' } }}/>
           </div>
-          <div className="input-row">
-            <select id="gender" value={gender} onChange={(e) => setGender(e.target.value)}>
-              <option value="">---</option>
+          <div className=" select input-row">
+
+          <FormControl sx={{ m: 1, minWidth: 90 }}>
+      <InputLabel id="demo-simple-select-autowidth-label">Gender</InputLabel>
+          <Select
+  labelId="demo-simple-select-label"
+  id="demo-simple-select"
+  label="gender"
+  value={gender} onChange={(e) => setGender(e.target.value)}
+>
+  <MenuItem value="MALE">MALE</MenuItem>
+  <MenuItem value="FEMALE">FEMALE</MenuItem>
+  <MenuItem value="OTHERS">OTHERS</MenuItem>
+</Select>
+</FormControl>
+
+
+
+
+
+
+            {/* <select id="gender" value={gender} onChange={(e) => setGender(e.target.value)}>
+              <option value="">gender</option>
               <option value="MALE">MALE</option>
               <option value="FEMALE">FEMALE</option>
               <option value="OTHERS">OTHERS</option>
-            </select>
+            </select> */}
             <TextField
               id="dateofbirth"
               label="DOB"
@@ -110,7 +165,7 @@ function Registration() {
               fullWidth
             />
             <TextField id="email" label="Email" type="email" variant="outlined" size="small" fullWidth />
-            {phoneNumberError && <p className="error">{phoneNumberError}</p>}
+            {/* {phoneNumberError && <p className="error">{phoneNumberError}</p>} */}
           </div>
           <div className="input-row">
             <TextField
